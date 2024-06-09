fad2387a03f26192553f8daaf6e0f552941f900e
chathura26322 <chathura26322@gmail.com>
2024-06-09 19:41:26 +0530

    Successfully added the API

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,36 +1,78 @@
 import "./Weather.css";
 import search_icon from "../assets/search.png";
 import clear_icon from "../assets/clear.png";
-// import cloud_icon from "../assets/cloud.png";
-// import drizzel_icon from "../assets/drizzel.png";
-// import rain_icon from "../assets/rain.png";
-// import snow_icon from "../assets/snow.png";
+import cloud_icon from "../assets/cloud.png";
+import drizzle_icon from "../assets/drizzle.png";
+import rain_icon from "../assets/rain.png";
+import snow_icon from "../assets/snow.png";
 import wind_icon from "../assets/wind.png";
 import humidity_icon from "../assets/humidity.png";
-// import { WiDayCloudy } from "react-icons/wi";
+import { useEffect, useState } from "react";
 
 const Weather = () => {
+  const [weatherData, setWeatherData] = useState(false);
+
+  const allIcons = {
+    "01d": clear_icon,
+    "01n": clear_icon,
+    "02d": cloud_icon,
+    "02n": cloud_icon,
+    "03d": cloud_icon,
+    "03n": cloud_icon,
+    "04d": drizzle_icon,
+    "04n": drizzle_icon,
+    "09d": rain_icon,
+    "09n": rain_icon,
+    "10d": rain_icon,
+    "10n": rain_icon,
+    "13d": snow_icon,
+    "13n": snow_icon,
+  };
+  const search = async (city) => {
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${
+        import.meta.env.VITE_APP_ID
+      }`;
+
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      const icon = allIcons[data.weather[0].icon] || clear_icon;
+      setWeatherData({
+        humidity: data.main.humidity,
+        tempearture: Math.floor(data.main.temp),
+        windSpeed: data.wind.speed,
+        location: data.name,
+        icon: icon,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    search("Colombo");
+  }, []);
   return (
     <div className="weather">
       <div className="search-bar">
         <input type="text" placeholder="Search" />
         <img src={search_icon} alt="" />
       </div>
-      <img src={clear_icon} alt="" className="weather-icon" />
-      <p className="temperature">16ºc</p>
-      <p className="location">London</p>
+      <img src={weatherData.icon} alt="" className="weather-icon" />
+      <p className="temperature">{weatherData.tempearture}ºc</p>
+      <p className="location">{weatherData.location}</p>
       <div className="weather-data">
         <div className="col">
           <img src={humidity_icon} alt="" />
           <div>
-            <p>91 %</p>
+            <p>{weatherData.humidity} %</p>
             <span>Humidity</span>
           </div>
         </div>
         <div className="col">
           <img src={wind_icon} alt="" />
           <div>
-            <p>3.6 km/h</p>
+            <p>{weatherData.windSpeed} km/h</p>
             <span>Wind Speed</span>
           </div>
         </div>
